a6ba8296a4861052e1466b1c1806248bb182c72b
Miquel Angel Aloy Vich <40128163+maavcrusoe@users.noreply.github.com>
2023-06-26 14:56:16 +0200

    Update my.backup.php
    -add auto refresh
    -set red color on text if file dosn't update in 24h

--- module/views/my.backup.php
+++ module/views/my.backup.php
@@ -7,21 +7,30 @@
     use CDiv;
     use CButton;
     use CTag;
+   
+    // Set autorefresh time interval
+    $autorefresh = 30;
+
+    // Send HTTP refresh header
+    header("Refresh: $autorefresh");
 
     // local url to retreive file text
-    $url="http://ZABBIX:8080/get_files.php";
+    $url="http://srvbt-mon.tunel.local:8080/get_files.php";
     
     // token used to retreive file
-    $token="XXXXXXXX";
+    $token="WUipXlkmQljA2byYi52dRj7VeUs1eSliSEheVKjX9dVcoFTdZ8AbJp9UC49AyucC";
+
+    // Get current timestamp
+    $currentTimestamp = time();
 
     // Create CTable instance
     $table = new CTable();
 
     // Get Files
-    $directory = '/home/user/backup-config';
-    $files = array_diff(scandir($directory), array('..', '.'));
+    $directory = '/home/sistemas/backup-config';
+    $files = array_diff(scandir($directory), array('..', '.','executions.log'));
 
-    // Leer y mostrar el contenido del archivo
+    // Read and print content of log file
     $logFileName = $directory . '/executions.log';
     $logFile = fopen($logFileName, 'r');
     $content = fread($logFile, filesize($logFileName));
@@ -45,18 +54,39 @@
         $link = (new CButton())
             ->addClass('myButton')
             ->addItem(
-                (new CLink($f, $get_url))
-                    ->setAttribute('style', 'text-align: center; color: white; background-color: green; padding: 10px; border-radius: 5px; border: none;')
+                (new CLink('Show config', $get_url))
+                    ->setAttribute('style', 'text-align: center; color: white; background-color: #5bbbbc; padding: 10px; border-radius: 5px; border: none;')
             );
 
         // make last update date readable
-        $updateDate = date("F d Y H:i:s.", filemtime($directory . "/" . $f));
+        $updateDate = date("F d Y H:i:s", filemtime($directory . "/" . $f));
+
+        // Convert last update date to timestamp
+        $lastUpdateTimestamp = date(filemtime($directory . "/" . $f));
+
+        // debug
+        //echo ('current'.$currentTimestamp . ' - ');
+        //echo ('last'. $lastUpdateTimestamp . ' - ');
+        //$diff = $currentTimestamp - $lastUpdateTimestamp;
+        //$time = 25*60*60;
+        //echo ('diff'. $diff);
+        //echo ('time'. $time);
+
+        // Check if last update time is updated recently or not
+        if (($currentTimestamp - $lastUpdateTimestamp > 24 * 60 * 60) || date("Y-m-d", $lastUpdateTimestamp) != date("Y-m-d")) {
+            // Set red color style to col
+            $updateDateCol = (new CCol($updateDate. ' >> This device is not sync'))->setAttribute('style', 'text-align: center; padding: 10px 0; color: red;');
+        } else {
+            // Set normal style
+            $updateDateCol = (new CCol($updateDate))->setAttribute('style', 'text-align: center; padding: 10px 0;');
+        }
 
         // add rows to table
         $table->addRow([
             (new CCol($f))->setAttribute('style', 'text-align: center; padding: 10px 0;'),
             (new CCol($link))->setAttribute('style', 'text-align: center; padding: 10px 0;'),
-            (new CCol($updateDate))->setAttribute('style', 'text-align: center; padding: 10px 0;')
+            //(new CCol($updateDate))->setAttribute('style', 'text-align: center; padding: 10px 0;')
+            $updateDateCol
         ]);
     }
 
@@ -64,13 +94,14 @@
     $page = new CHtmlPage();
 
     // Set Title
-    $page->setTitle(_('Backup - Networking'));
+    $page->setTitle('Backup - Networking');
 
     // Create div with info content at the top
     $divdirectory = (new CDiv())
+        ->addItem((new CDiv('Last check: ' . date("F d Y H:i:s",$currentTimestamp)))->setAttribute('style', 'display: block; font-size: 20px;'))
         ->addItem('Directory: ' . $directory)
-        ->addItem((new CDiv('Items: ' . count($files)))->setAttribute('style', 'display: block;'))
-        ->setAttribute('style', 'margin-bottom: 20px; background-color: #2b2b2b; padding: 10px;');
+        ->addItem((new CDiv('Items: ' . count($files)))->setAttribute('style', 'display: block; font-size: 14px;'))
+        ->setAttribute('style', 'margin-bottom: 20px; background-color: #2b2b2b; padding: 10px;font-size: 14px;');
   
     $divTextLog = (new CDiv())
         ->addItem('LOG')
